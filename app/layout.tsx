7eae74f98cc4b01d4b0b4c0ef68195e840c35e5d
CodigoTotal <85316618+CodigoTotal01@users.noreply.github.com>
2024-06-11 10:15:27 -0500

    finalizao

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { Inter } from "next/font/google";
+
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
